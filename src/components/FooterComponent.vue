946e23470bd20197ce2e48884a3a3ad46ef39bce
Willy <willy718066@gmail.com>
2024-04-15 15:32:34 +0800

    footer link add

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -9,17 +9,31 @@
         <a class="text-white h4" href="/">SOUXI</a>
         <ul class="d-flex list-unstyled mb-0 h4">
           <li>
-            <a href="#" class="text-white mx-3"
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noreferrer noopenner"
+              class="text-white mx-3"
               ><i class="fab fa-facebook"></i
             ></a>
           </li>
           <li>
-            <a href="#" class="text-white mx-3"
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noreferrer noopenner"
+              class="text-white mx-3"
               ><i class="fab fa-instagram"></i
             ></a>
           </li>
           <li>
-            <a href="#" class="text-white ms-3"><i class="fab fa-line"></i></a>
+            <a
+              href="https://line.me/tw/"
+              target="_blank"
+              rel="noreferrer noopenner"
+              class="text-white ms-3"
+              ><i class="fab fa-line"></i
+            ></a>
           </li>
         </ul>
       </div>
